e37b4c25d0367f56182258e6cda456e5de6ff464
José Alfredo Arroyo Gómez <arroyo.alfredo.1pm@gmail.com>
2024-05-25 01:25:26 -0600

    Update Hero, Change Image And Text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,13 +10,13 @@ const Hero = () => {
 						The <span className="text-orange-400">Best</span>
 					</h1>
 					<h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
-						<span className="text-orange-400">Foods</span> Delivered
+						<span className="text-orange-400">Mexican Foods</span> Delivered
 					</h1>
 				</div>
 
 				<img
                     className="w-full max-h-[500px] object-cover"
-					src="https://images.pexels.com/photos/1639565/pexels-photo-1639565.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+					src="https://images.pexels.com/photos/2092507/pexels-photo-2092507.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 					alt="/"
 				/>
 			</div>
